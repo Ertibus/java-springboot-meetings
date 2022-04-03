0be7d7e1d364666a21f6636b1f4b27c8535d1de0
Emilis Margevičius <gameciopath@gmail.com>
2022-04-03 08:55:59 +0300

    Spring Boot Init

---
+++ src/main/java/lt/visma/geeks/intern/meetings/InternalMeetingsApplication.java
@@ -0,0 +1,13 @@
+package lt.visma.geeks.intern.meetings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternalMeetingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternalMeetingsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/visma/geeks/intern/meetings/InternalMeetingsApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.visma.geeks.intern.meetings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternalMeetingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
